8654e2a98851d30babdd67afcb738ebd96bfdcc4
гк <orametow@mail.ru>
2022-09-26 18:58:40 +0300

    Hello

---
+++ src/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+   int points = 20; // byte, short, int, long,
+  if (points<38) System.out.println("Your mark is 3");
+    else if (points<50)}System.out.println("Your mark is 4");
+    else System.out.println("Your mark is 5");
+}
